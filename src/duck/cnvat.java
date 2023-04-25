cbc9582d983fb5c9b540621da656df9708e8b7a9
lee mih duwk <117137275+rhymLele@users.noreply.github.com>
2023-04-25 13:41:24 +0700

    update cnvat

--- src/duck/cnvat.java
+++ src/duck/cnvat.java
@@ -5,6 +5,14 @@ public class cnvat {
 	public double x,y,vx,vy;
 	public int w,h;
 	public Block block;
+	public void setVx()
+	{
+		 vx*=-1;
+	}
+	public void setVy()
+	{
+		vy*=-1;
+	}
 	public cnvat(double x,double y,Block b,double vx,double vy)
 	{
 		this.x=x;
@@ -20,6 +28,8 @@ public class cnvat {
 	{
 		x+=vx;
 		y+=vy;
+		//||(nguoichoi.x<x+w&&nguoichoi.x+nguoichoi.h>x)
+		//||(nguoichoi.y<y+h&&nguoichoi.y+nguoichoi.h>y)
 		if(x<0||x>=Duck.W-w) vx*=-1;
 		if(y<0||y>=Duck.H-h) vy*=-1;
 		
